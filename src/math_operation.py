2a427660894d532cd5e91f58d91b22ff1bfd536c
DreamDeliverer <64832344+DreamDeliverer@users.noreply.github.com>
2024-09-12 11:57:18 +0200

    Update math_operation.py

---
+++ src/math_operation.py
@@ -0,0 +1,9 @@
+from src.math_operation import add, sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(10,5) == 15
+    
+def test_sub():
+    assert sub(7-5) == 2
+    assert sub(13-5)== 8
